cd4e89325b745c920d95680a760cf69779ea6f2b
negar_bahadori <negari.virgo@gmail.com>
2014-06-05 01:30:42 +0430

    honey

--- app/views/public/honey.blade.php
+++ app/views/public/honey.blade.php
@@ -5,7 +5,19 @@
 
         <img src="/static/images/big_pic1.jpg" alt="" class="img3">
 
-        <!-- TODO : Edit here ! -->
+        <div class="honey-product grid_12">
+
+            <div class="product-img grid_4">
+            <img src="/static/images/page3_pic3.jpg"/></div>
+            <div class="product-desc grid_6">
+            <h3 class="product-title">honeye kheyli shakh</h3>
+                <div>
+            Quisque nestibulum libero nisl, porta vel, scelerisque eget,
+                malada at, mus eget nibh. Etiam cursus
+                leo vel metus. Nulla facilisi. Aenean nec eros. Vestibulum ante ipsum primis in faucibus orci
+                luctus.</div></div>
+
+        </div>
 
 
     </div>
